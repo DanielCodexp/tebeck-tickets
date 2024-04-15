9f58d28fee67cd4306e54899fab54a594c6883f4
Angel Daniel <116531997+DanielCodexp@users.noreply.github.com>
2024-04-15 00:18:32 -0500

    cambios

--- src/app/pages/products/components/print/print.component.ts
+++ src/app/pages/products/components/print/print.component.ts
@@ -121,8 +121,8 @@ export class PrintComponent {
 
   async imprimirTicket(selectedPrinterInfo: any): Promise<void> {
     console.log("Imprimiendo...");
-    const anchoEtiqueta = 38;
-    const largoEtiqueta = 38;
+    const anchoEtiqueta = 18;
+    const largoEtiqueta = 18;
 
     if (this.ticketImageElement && this.ticketImageElement.nativeElement) {
       const ticketImage = this.ticketImageElement.nativeElement;
