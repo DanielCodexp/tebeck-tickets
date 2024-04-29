b9e9d3d3e2ebbaa1313dd3462bee309e3fe93b28
Angel Daniel <116531997+DanielCodexp@users.noreply.github.com>
2024-04-28 20:30:23 -0500

    se ajustaron las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,21 +11,21 @@ import { SignInGuard } from './guards/sign-in.guard';
             {
                 path: '', component: AppLayoutComponent, canActivate: [AuthGuard],
                 children: [
-                    { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-                    { path: 'admins', loadChildren: () => import('./pages/admins/admins.module').then(m => m.AdminsModule) },
                     { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
-                    { path: 'clients', loadChildren: () => import('./pages/clients/clients.module').then(m => m.ClientsModule)},
-                    { path: 'suppliers', loadChildren:() => import('./pages/suppliers/suppliers.module').then(m => m.SuppliersModule) },
-                    { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
-                    { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
-                    { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
-                    { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-                    { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
+                    // { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+                    // { path: 'admins', loadChildren: () => import('./pages/admins/admins.module').then(m => m.AdminsModule) },
+                    // { path: 'clients', loadChildren: () => import('./pages/clients/clients.module').then(m => m.ClientsModule)},
+                    // { path: 'suppliers', loadChildren:() => import('./pages/suppliers/suppliers.module').then(m => m.SuppliersModule) },
+                    // { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
+                    // { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
+                    // { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
+                    // { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
+                    // { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
                 ]
             },
             { path: 'auth', canActivate: [SignInGuard], loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
             { path: 'notfound', component: NotfoundComponent },
-            { path: '**', redirectTo: '/notfound' },
+            { path: '**', redirectTo: '/products' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
     ],
     exports: [RouterModule]
